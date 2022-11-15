09467a09321e92db1285ee031741ccc3807b6da3
jahernando <hernando.joseangel@gmail.com>
2022-11-15 21:59:16 +0100

    connect, event_summary

--- src/thekla.jl
+++ src/thekla.jl
@@ -3,7 +3,7 @@ using DataFrames
 using Clouds
 using LinearAlgebra
 
-export _coors, _thekla
+export _coors, _thekla, event_summary
 
 function _distance(coors0, coors1)
 	x0 = reduce(hcat, coors0)
@@ -47,6 +47,16 @@ function _dfnodes(xnd)
 
 end
 
+function _connect(contents, emin = 1e-3)
+	ene0 = sum(contents)
+	sel = contents .== 0.0
+	contents[sel] .= emin
+	ene1 = sum(contents)
+	factor = ene1 > 0 ? ene0/ene1 : 1.0
+	contents = factor .* contents
+	return contents
+end
+
 function _coors(idf, steps; nsteps = 1)
 
 	coors    = Tuple((idf[!, :x], idf[!, :y], idf[!, :z]))
@@ -70,9 +80,10 @@ function _thekla(idf, steps;
 	 			 nsteps = 1, cellnode = false)
 
 	coors, contents, xsteps = _coors(idf, steps; nsteps = nsteps)
+	#contents = _connect(contents) # fix to connect empty energy voxels
 
 	cl, nd, graph, edges = clouds(coors, contents, xsteps;
-	  							cellnode = cellnode)
+	  							  cellnode = cellnode)
 	clabel = label_cell(edges, cl.cells, coors, idf.segclass)
 	nlabel = label_node(cl.node, clabel)
 
@@ -85,23 +96,36 @@ function _thekla(idf, steps;
 	return dd
 end
 
-datadir       = "/Users/hernando/work/investigacion/NEXT/data/NEXT100/"
-filename      = "bb0nu/v2/beersheba_fixed_label_1_0nubb.h5"
-filenames     = Tuple(string("bb0nu/v2/beersheba_fixed_label_", i, "_0nubb.h5")
+datadir        = "/Users/hernando/work/investigacion/NEXT/data/NEXT100/"
+ifiles         = Dict()
+ifiles[:bb0nu] = Tuple(string("bb0nu/v2/beersheba_fixed_label_", i, "_0nubb.h5")
 				for i in 1:249)
+ifiles[:Bi214] = Tuple(string("Bi/Beersheba/fixed_label/beersheba_label_", i,
+				"_214Bi.h5") for i in 1:310)
+ofiles         = Dict()
+ofiles[:bb0nu] = "bb0nu/Thekla/thekla_nodes_noconnect"
+ofiles[:Bi214] = "Bi/Thekla/thekla_nodes_noconnect"
 #df, mc, steps = load_data(string(datadir, filename));
 
 
-function thekla(datadir, filenames;
-	 			ofilename = "bb0nu/v2/thekla_nodes_nfiles50",
-				reco = true, cellnode = false, nsteps = 1)
+function thekla(; data   = :bb0nu,
+	 			nfiles = -1,
+				reco   = true,
+				cellnode = false,
+				nsteps = 1)
+
+	filenames = nfiles > 0 ? ifiles[data][1:nfiles] : ifiles[data]
+	nfiles = length(filenames)
+	println("Total number of input files ", nfiles)
 
 	datatype = reco ? "reco" : "mc"
-	algoname = cellnode ? "paulina" : "clouds"
-	ofile    = string(ofilename,  "_", algoname,
+	algoname = cellnode ? "breadth" : "clouds"
+
+	ofilename = ofiles[data]
+	ofile     = string(ofilename, "_nfiles", nfiles, "_", algoname,
 						"_", datatype,
 					 	"_nsteps", nsteps,".csv")
-	println(ofile)
+	println("Output filename ", ofile)
 
 	nevts = 0
 	dfout = DataFrame()
@@ -128,6 +152,73 @@ function thekla(datadir, filenames;
 
 	return dfout
 end
+
+#---- analysis
+
+function event_summary(df)
+	nevents = length(Set(df.event))
+	event   = zeros(Int64  , nevents)    # event number
+	energy  = zeros(Float64, nevents)    # energy of the event
+	ncloud  = zeros(Int64  , nevents)    # number of clouds (tracks)
+	nnodes  = zeros(Int64  , nevents)    # number of nodes
+	nextrs  = zeros(Int64  , nevents)    # number of extremes
+	eextr1  = zeros(Float64, nevents)    # energy of extreme 1 (most energetic)
+	eextr2  = zeros(Float64, nevents)    # energy of extreme 2
+	lextr1  = zeros(Int64  , nevents)    # label of the extreme 1
+	lextr2  = zeros(Int64  , nevents)    # label of extreme 2
+	dextr1  = zeros(Int64  , nevents)    # distance of the extreme 1 to a blob
+	dextr2  = zeros(Int64  , nevents)    # distance of the extreme 2 to a blob
+	nextrbs = zeros(Int64  , nevents)    # number of extremes labeled as blobs
+	nnodebs = zeros(Int64  , nevents)    # number of nodes labeled as blobs
+	enodeb1 = zeros(Float64, nevents)    # energy of the node labeled as blob 1-most energetic
+	enodeb2 = zeros(Float64, nevents)    # energy of the node labelled as blob 2
+	edf     = groupby(df, :event)
+	for (i, kdf) in enumerate(edf)
+		event[i]   = maximum(kdf.event)
+		energy[i]  = sum(kdf.contents)
+		ncloud[i]  = maximum(kdf.cloud)
+		nnodes[i]  = length(kdf.event)
+		idextr     = findall(x -> x == 1, kdf.extreme)
+		idblobs    = findall(x -> x == 3, kdf.label)
+		nextrs[i]  = length(idextr)
+		nnodebs[i] = length(idblobs)
+		if (length(idextr) == 2)
+			eextr   = kdf.contents[idextr]
+			k1, k2  = argmax(eextr), argmin(eextr)
+			i1, i2  = idextr[k1], idextr[k2]
+			eextr1[i]  = maximum(eextr)
+			eextr2[i]  = minimum(eextr)
+			lextr1[i]  = kdf.label[i1]
+			lextr2[i]  = kdf.label[i2]
+			dextr1[i]  = kdf.disttoblob[i1]
+			dextr2[i]  = kdf.disttoblob[i2]
+			nextrbs[i] = (lextr1[i] == 3) + (lextr2[i] == 3)
+		end
+		if (length(idblobs) >= 2)
+			enodebs = sort(kdf.contents[idblobs], rev = true)
+			enodeb1[i] = enodebs[1]
+			enodeb2[i] = enodebs[2]
+		end
+	end
+	dd = Dict(:event    => event,
+			  :energy   => energy,
+			  :nclouds  => ncloud,
+		      :nnodes   => nnodes,
+		      :nextrs   => nextrs,
+		      :eextr1   => eextr1,
+		      :eextr2   => eextr2,
+		      :lextr1   => lextr1,
+		      :lextr2   => lextr2,
+			  :dextr1   => dextr1,
+		      :dextr2   => dextr2,
+		      :nextrbs  => nextrbs,
+		      :nnodesbs => nnodebs,
+		      :enodeb1  => enodeb1,
+		      :enodeb2  => enodeb2)
+	dd = DataFrame(dd)
+return dd
+end
+
 
 #
 # function run(filename, config)
